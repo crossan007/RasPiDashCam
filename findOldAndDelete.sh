0fef685b59b96545251bf621c424acfc7f5acf66
crossan007 <crossan007@gmail.com>
2015-07-18 10:06:46 -0400

    Added Loop
    script will now loop, deleting files until the limit has been satisfied

--- findOldAndDelete.sh
+++ findOldAndDelete.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 TARGET=/var/cache/avconv/
-DISKSPACE=`df -H /dev/root | sed '1d' | awk '{print $5}' | cut -d'%' -f1`
-ALERT=70
-logger "${DISKSPACE}% used.  Limit is ${ALERT}%"
-if [ ${DISKSPACE} -ge ${ALERT} ]; then
-    logger "Disk utilization over limit.  Deleting 1 oldest files from $TARGET"
-    ls -A1 $TARGET  | head -1 | xargs -I '{}' -- sh -c "logger Deleting: $TARGET/'{}' && rm $TARGET/'{}'"
-    exit
-  else
-    logger "Disk itilization under limit.  No deletions necessary."
-
-fi
+while : ; do
+	DISKSPACE=`df -H /dev/root | sed '1d' | awk '{print $5}' | cut -d'%' -f1`
+	ALERT=70
+	logger "${DISKSPACE}% used.  Limit is ${ALERT}%"
+	if [ ${DISKSPACE} -ge ${ALERT} ]; then
+	    	logger "Disk utilization over limit.  Deleting 1 oldest files from $TARGET"
+	    	ls -A1 $TARGET  | head -1 | xargs -I '{}' -- sh -c "logger Deleting: $TARGET/'{}' && rm $TARGET/'{}'"
+	  else
+	    	logger "Disk utilization under limit.  No deletions necessary."
+		exit	
+	fi
+done
+logger "Done cleanup"
