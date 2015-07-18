cc2ebc420582a9cc73f38b262d37db003e332e11
crossan007 <crossan007@gmail.com>
2015-07-18 09:57:39 -0400

    Altered Behavior
    fixed delete behavior and conditions.

--- findOldAndDelete.sh
+++ findOldAndDelete.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+TARGET=/var/cache/avconv/
 DISKSPACE=`df -H /dev/root | sed '1d' | awk '{print $5}' | cut -d'%' -f1`
-ALERT=80
+ALERT=70
 logger "${DISKSPACE}% used.  Limit is ${ALERT}%"
 if [ ${DISKSPACE} -ge ${ALERT} ]; then
-    logger "Disk utilization over limit.  Deleting 10 oldest files from /var/cache/avconv"
-    find /var/cache/avconv -type f | tail -n -1 | xargs rm
+    logger "Disk utilization over limit.  Deleting 1 oldest files from $TARGET"
+    ls -A1 $TARGET  | head -1 | xargs -I '{}' -- sh -c "logger Deleting: $TARGET/'{}' && rm $TARGET/'{}'"
     exit
   else
     logger "Disk itilization under limit.  No deletions necessary."
