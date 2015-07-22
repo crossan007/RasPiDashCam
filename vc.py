fc82c1daffeb02756ce29afd414c1a4dc6d840ef
crossan007 <crossan007@gmail.com>
2015-07-21 21:03:49 -0400

    Moved input and output to separate threads

--- vc.py
+++ vc.py
@@ -30,20 +30,11 @@ import syslog
 date = " "
 temp = " "
 exitFlag = 0
-w = 640
-h = 480
-nbytes = 3*w*h
-i=0
-font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf",14)
 
-class updateVars (threading.Thread):
-    def __init__(self, threadID, name ,q):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.q = q
-        self.setDaemon(True)
-    def run(self):
+q = Queue.Queue()
+
+
+def updateVars():
         global date
         global temp
         syslog.syslog("Starting "+self.name)
@@ -55,61 +46,65 @@ class updateVars (threading.Thread):
             time.sleep(1)
         syslog.syslog("Stopping "+self.name)
 
-class processFrames (threading.Thread):
-    def __init__(self, threadID,name,q):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.q = q
-        self.setDaemon(True)
-    def run(self):
-        global w, h, nBytes,i
-        global inputPipeline
-        global outputPipeline
-        global font
+
+def getFrames():
+    i=0
+    w = 640
+    h = 480
+    nbytes = 3*w*h
+    font = ImageFont.truetype("/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans-Bold.ttf",14)
+    command = "/usr/bin/avconv -f video4linux2 -r 15 -input_format yuyv422 -video_size 640x480 -i /dev/video0 -f rawvideo -pix_fmt rgb24 pipe: "
+    inputPipeline = sp.Popen(shlex.split(command), stdout = sp.PIPE, stderr = sp.PIPE, stdin = DEVNULL )
+    while True:
+        if i % 100 == 0:
+            syslog.syslog( "Starting processing frame %d" %i)
+        try:
+            raw_image = inputPipeline.stdout.read(nbytes)
+        except IOError as err:
+                ffmpeg_error = inputPipeline.stderr.read()
+                syslog.syslog (ffmpeg_error) 
+        except: # catch *all* exceptions
+            e = sys.exc_info()[0]
+            syslog.syslog("Error: %s" % e )
+        if len(raw_image) != nbytes:
+            syslog.syslog("Warning, not reading right # byes, %d, %d" % ( len(raw_image), nbytes) )
+        if i % 100 == 0:
+            syslog.syslog("Manipulating frame %d" %i)
+        img = Image.frombuffer('RGB', (640,480), raw_image, 'raw', 'RGB', 0, 1)
+        draw=ImageDraw.Draw(img)
+        draw.text((0, 460), temp ,(255,255,0),font=font)
+        draw.text((160, 460), date ,(255,255,0),font=font)
+        draw.text((580, 460), str(i) ,(255,255,0),font=font)
+        draw = ImageDraw.Draw(img)
+        q.put(img.tostring())
+        del img
+        del draw
+        del raw_image
+        gc.collect()
+        i+=1
         
-        while True:
-            if i % 10 == 0:
-                syslog.syslog( "Starting processing frame %d" %i)
-            try:
-                raw_image = inputPipeline.stdout.read(nbytes)
-                syslog.syslog( "frame %d read" %i)
-            except IOError as err:
-                    ffmpeg_error = inputPipeline.stderr.read()
-                    syslog.syslog (ffmpeg_error) 
-            except: # catch *all* exceptions
-                e = sys.exc_info()[0]
-                syslog.syslog("Error: %s" % e )
-                    
-            #print "%s" % raw_image
-            if len(raw_image) != nbytes:
-                print "Warning, not reading right # byes, %d, %d" % ( len(raw_image), nbytes) 
-            if i % 10 == 0:
-                syslog.syslog( "Manipulating frame %d" %i)
-            print "Read %d bytes" % len(raw_image)
-            img = Image.frombuffer('RGB', (640,480), raw_image, 'raw', 'RGB', 0, 1)
-            draw=ImageDraw.Draw(img)
-            draw.text((0, 460), temp ,(255,255,0),font=font)
-            draw.text((160, 460), date ,(255,255,0),font=font)
-            draw.text((580, 460), str(i) ,(255,255,0),font=font)
-            draw = ImageDraw.Draw(img)
-            #img.save("a_test.jpg")
-            #print "%s" % img.tostring()
-            if i % 10 == 0:
-                syslog.syslog( "Starting push frame %d" %i)
-            try:
-                outputPipeline.stdin.write(img.tostring())
-            except IOError as err:
-                ffmpeg_error = outputPipeline.stderr.read()
-                syslog.syslog(ffmpeg_error)
-            if i % 10 == 0:
-                syslog.syslog( "Done with frame %d" %i)
-            i+=1
-            del img
-            del draw
-            del raw_image
-            gc.collect()
-        
+def putFrames():
+    i=0
+    command2 = "/usr/bin/avconv -f rawvideo -pix_fmt rgb24 -video_size 640x480 -r 15 -i pipe: -vf \"setpts=0.1*PTS\" -c:v libx264 -preset ultrafast -tune film -map 0 -f flv -f segment -segment_time 90 -r 90 \"/var/cache/avconv/"+date+"-%03d.flv\""
+    outputPipeline = sp.Popen(shlex.split(command2), stdout = sp.PIPE, stderr = sp.PIPE, stdin = sp.PIPE )
+    while True:
+        img=q.get()
+        if i % 10 == 0:
+            syslog.syslog( "Starting push frame %d" %i)
+        try:
+            outputPipeline.stdin.write(img)
+        except IOError as err:
+            ffmpeg_error = outputPipeline.stderr.read()
+            syslog.syslog(ffmpeg_error)
+        except: # catch *all* exceptions
+            e = sys.exc_info()[0]
+            syslog.syslog("Error: %s" % e )
+            ffmpeg_error = outputPipeline.stdout.read()
+            syslog.syslog(ffmpeg_error)
+        if i % 10 == 0:
+            syslog.syslog( "Done with frame %d" %i)
+        i+=1
+    
     
 try:
     from subprocess import DEVNULL  # py3k
@@ -118,30 +113,27 @@ except ImportError:
 
 os.system("pkill avconv");
 
-command = "/usr/bin/avconv -f video4linux2 -r 15 -input_format yuyv422 -video_size 640x480 -i /dev/video0 -f rawvideo -pix_fmt rgb24 pipe: "
-inputPipeline = sp.Popen(shlex.split(command), stdout = sp.PIPE, stderr = sp.PIPE, stdin = DEVNULL )
 
 ts = time.time()
 date = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d-%H%M%S')
 
 
-#-f segment -segment_time 90 -r 90
-
-command2 = "/usr/bin/avconv -f rawvideo -pix_fmt rgb24 -video_size 640x480 -r 15 -i pipe: -vf \"setpts=0.1*PTS\" -c:v libx264 -preset ultrafast -tune film -map 0 -f flv \"/var/cache/avconv/"+date+"-%03d.flv\""
-outputPipeline = sp.Popen(shlex.split(command2), stdout = sp.PIPE, stderr = sp.PIPE, stdin = sp.PIPE )
+#
 
 
-thread = updateVars(1,"UpdateVars",Queue.Queue(3))
+
+thread = threading.Thread(target=getFrames)
+thread.daemon = True
 thread.start()
 
-thread2 = processFrames(2, "ProcessFrames", Queue.Queue(3))
+thread2 = threading.Thread(target=putFrames)
+thread2.daemon = True
 thread2.start()
 
 while True:
     time.sleep(10)
     syslog.syslog("vc.py still alive ")
-    
-
+    syslog.syslog("queue size: %d" % q.qsize())
     
 exitFlag=1
 inputPipeline.kill()
